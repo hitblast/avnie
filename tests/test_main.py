b15e7cd479b2647f584556bb185c6934cd6e8183
HitBlast <hitblastlive@gmail.com>
2024-08-06 15:47:43 +0600

    updated unit tests to comply with avro.py package

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,17 +6,47 @@ from typer.testing import CliRunner
 # Import the module containing the CLI commands.
 from avnie.main import app
 
-
 # Test cases.
-def test_parse_english_to_bengali():
-    runner = CliRunner()
-    result = runner.invoke(app, ["parse", "hyalO"])
-    assert result.exit_code == 0
-    assert result.stdout.strip() == "হ্যালো"
+# This uses the typer.testing.CliRunner class to test the CLI commands.
+runner = CliRunner()
 
 
-def test_reverse_bengali_to_english():
-    runner = CliRunner()
-    result = runner.invoke(app, ["reverse", "আমি বাংলায় গান গাই"])
+def test_conversion_bijoy_func():
+    result = runner.invoke(app, ["tobijoy", "আমি বাংলায় গান গাই;"])
     assert result.exit_code == 0
-    assert result.stdout.strip() == "ami banglay gan gai"
+    assert result.stdout.strip() == "Avwg evsjvq Mvb MvB;"
+
+
+def test_full_sentences():
+    result = runner.invoke(app, ["parse", "ami banglay gan gai;"])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == "আমি বাংলায় গান গাই;"
+
+    result = runner.invoke(
+        app, ["parse", "ami banglar gan gai.", "ami amar amike cirodin ei banglay khu^je pai."]
+    )
+    assert result.exit_code == 0
+    assert result.stdout.strip() == "আমি বাংলার গান গাই।\nআমি আমার আমিকে চিরদিন এই বাংলায় খুঁজে পাই।"
+
+    result = runner.invoke(app, ["parse", "--bijoy", "ami banglay gan gai;"])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == "Avwg evsjvq Mvb MvB;"
+
+    result = runner.invoke(
+        app, ["parse", "--bijoy", "ami banglar gan gai.", "ami amar amike cirodin ei banglay khu^je pai!"]
+    )
+    assert result.exit_code == 0
+    assert result.stdout.strip() == "Avwg evsjvi Mvb MvB|\nAvwg Avgvi Avwg‡K wPiw`b GB evsjvq Lyu‡R cvB!"
+
+    result = runner.invoke(app, ["reverse", "আমি বাংলায় গান গাই।"])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == "ami banglay gan gai."
+
+    result = runner.invoke(
+        app, ["reverse", "রহিম, তোমাকে করিম ডাকছে। এখন কি রওনা দেবে?", "রওনা দিলে আমাকে বলে যেও।"]
+    )
+    assert result.exit_code == 0
+    assert (
+        result.stdout.strip()
+        == "rohim, tomake korim dakche. ekhon ki rowna debe?\nrowna dile amake bole zew."
+    )
